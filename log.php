f69222f580bfbbd6a29032f9857f317a825fe503
Apex Admin <admin@apexaccounting.com>
2026-05-30 22:30:26 +0530

    Update log.php to also dump Nginx config to debug static file routing issues

--- log.php
+++ log.php
@@ -1,12 +1,17 @@
 <?php
+header('Content-Type: text/plain');
+
+echo "--- NGINX CONFIG ---\n";
+echo shell_exec('cat /opt/docker/etc/nginx/vhost.conf 2>&1');
+echo shell_exec('cat /opt/docker/etc/nginx/vhost.common.d/*.conf 2>&1');
+
+echo "\n--- LARAVEL LOG ---\n";
 $logFile = __DIR__ . '/storage/logs/laravel.log';
 if (!file_exists($logFile)) {
-    echo "No log file found at $logFile";
-    exit;
+    echo "No log file found at $logFile\n";
+} else {
+    $fp = fopen($logFile, 'r');
+    fseek($fp, -10000, SEEK_END);
+    echo fread($fp, 10000);
+    fclose($fp);
 }
-$fp = fopen($logFile, 'r');
-fseek($fp, -10000, SEEK_END);
-$content = fread($fp, 10000);
-fclose($fp);
-header('Content-Type: text/plain');
-echo $content;
