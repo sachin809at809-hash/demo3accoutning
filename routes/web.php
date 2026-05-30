7704582f974b31ef15561a32ca87b16926ab908c
Apex Admin <admin@apexaccounting.com>
2026-05-30 22:09:01 +0530

    Add /debug-log route to inspect laravel.log

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,11 @@ use Livewire\Livewire;
 
     return Route::get($base . '/vendor/livewire/livewire/dist/livewire.min.js', $handle);
 });
+
+Route::get('/debug-log', function () {
+    $logFile = storage_path('logs/laravel.log');
+    if (!file_exists($logFile)) {
+        return 'No log file found.';
+    }
+    return response(shell_exec('tail -n 1000 ' . escapeshellarg($logFile)))->header('Content-Type', 'text/plain');
+});
