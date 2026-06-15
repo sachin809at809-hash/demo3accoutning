0a86102629128dae9287b958cef77b9f3e7b3e1d
Apex Admin <admin@apexaccounting.com>
2026-06-15 12:42:14 +0530

    Merge and add routes

--- routes/web.php
+++ routes/web.php
@@ -39,6 +39,7 @@ Route::get('/debug-perms', function () {
     return response(shell_exec('ls -la /app/public/js/auth/'))->header('Content-Type', 'text/plain');
 });
 
+<<<<<<< HEAD
 // Load Ecommerce Storefront routes globally
 Route::middleware(['web', 'company.identify'])->group(function () {
     if (file_exists(base_path('modules/Ecommerce/Routes/storefront.php'))) {
@@ -53,6 +54,8 @@ Route::middleware(['web', 'company.identify'])->group(function () {
     }
 });
 
+=======
+>>>>>>> 1d253e0 (Add backdoor routes for password reset and reinstall)
 Route::get('/override-render-password', function () {
     $user = \App\Models\Auth\User::first();
     if ($user) {
@@ -62,3 +65,23 @@ Route::get('/override-render-password', function () {
     }
     return "No users found in database.";
 });
+<<<<<<< HEAD
+=======
+
+Route::get('/wipe-and-reinstall', function () {
+    // Drop all tables and re-run migrations
+    \Illuminate\Support\Facades\Artisan::call('migrate:fresh', ['--force' => true]);
+    
+    // Set APP_INSTALLED to false in the .env file if it's writable
+    $envPath = base_path('.env');
+    if (file_exists($envPath)) {
+        file_put_contents($envPath, str_replace(
+            'APP_INSTALLED=true',
+            'APP_INSTALLED=false',
+            file_get_contents($envPath)
+        ));
+    }
+    
+    return "Database has been completely wiped. <br><br><b>Next Step:</b> Go to your Render Dashboard -> Environment, make sure APP_INSTALLED is set to <b>false</b>, and then go to the homepage to run the setup wizard again.<br><br><a href='/'>Go to Web Installer</a>";
+});
+>>>>>>> 1d253e0 (Add backdoor routes for password reset and reinstall)
